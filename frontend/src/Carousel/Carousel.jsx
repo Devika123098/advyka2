26d640f14fb522812f7e8b740d32b659301927bb
alanreji2004 <alenreji2004@gmail.com>
2025-03-02 00:34:13 +0530

    added buttons in carousel

--- frontend/src/Carousel/Carousel.jsx
+++ frontend/src/Carousel/Carousel.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import Slider from "react-slick";
+import { IoTicketSharp } from "react-icons/io5"; // Import ticket icon
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./Carousel.module.css"; 
-import proshow1 from "../assets/proshow1.webp"
+import proshow1 from "../assets/proshow1.webp";
 
 const proshows = [
   {
     id: 1,
     image: proshow1,
+    date: "March 10, 2025",
     link: "/proshows",
   },
   {
     id: 2,
     image: proshow1,
+    date: "March 11, 2025",
     link: "/proshows",
   },
   {
     id: 3,
     image: proshow1,
+    date: "March 12, 2025",
     link: "/proshows",
   }
 ];
@@ -48,12 +52,15 @@ const Carousel = () => {
         {proshows.map((show) => (
           <div key={show.id} className={styles.carouselCard}>
             <img src={show.image} alt={`Proshow ${show.id}`} className={styles.image} />
-            <button 
-              onClick={() => window.location.href = show.link} 
-              className={styles.ticketButton}
-            >
-              Get Tickets
-            </button>
+            <div className={styles.infoOverlay}>
+              <p className={styles.eventDate}>{show.date}</p>
+              <button 
+                onClick={() => window.location.href = show.link} 
+                className={styles.ticketButton}
+              >
+                <IoTicketSharp className={styles.ticketIcon} /> Get Tickets
+              </button>
+            </div>
           </div>
         ))}
       </Slider>
