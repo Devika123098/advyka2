dc24efd3bc80affc273be116d7b219ca7146da0c
Aravind <aromalaravind0@gmail.com>
2025-03-11 01:29:31 +0530

    pastshows design without blue BG

--- frontend/src/PastEvents/Pastevents.jsx
+++ frontend/src/PastEvents/Pastevents.jsx
@@ -5,10 +5,8 @@ import iris from "../assets/iris.png"
 const Pastevents = () => {
   return (
     <div>
-      <div className="glimpse"></div>
        <h2 style={{ textAlign: "center", color: "white", marginBottom: "10px",marginTop:"10px" }}>
        A Glimpse of Advyka'24</h2>      
-      <div className="tr"></div>
       <div className="dec"><img width={700} src={iris} alt="lol" /></div>
       <Carousel3D /> 
     </div>
