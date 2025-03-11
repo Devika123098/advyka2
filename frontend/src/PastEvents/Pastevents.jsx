f66d7c8d3b5554b02221f0f3276dfa6996779727
alanreji2004 <alenreji2004@gmail.com>
2025-03-11 07:26:56 +0530

    fixed some margin issue

--- frontend/src/PastEvents/Pastevents.jsx
+++ frontend/src/PastEvents/Pastevents.jsx
@@ -4,8 +4,9 @@ import Carousel3D from "./Carousel3D";
 import iris from "../assets/iris.png"
 const Pastevents = () => {
   return (
+
     <div>
-       <h2 style={{ textAlign: "center", color: "white", marginBottom: "10px",marginTop:"10px" }}>
+       <h2 style={{ textAlign: "center", color: "white", marginBottom: "0px",marginTop:"30vh" }}>
        A Glimpse of Advyka'24</h2>      
       <div className="dec"><img width={700} src={iris} alt="lol" /></div>
       <Carousel3D /> 
