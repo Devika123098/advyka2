9e890ad70e50170c2b56646a06fbe62d25e47708
alanreji2004 <alenreji2004@gmail.com>
2025-03-18 10:07:28 +0530

    added partners

--- frontend/src/Partners/Partners.jsx
+++ frontend/src/Partners/Partners.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import styles from "./Partners.module.css";
 import makemypasslogo from "../assets/makemypasslogo.webp"; 
-
+import concoctlogo from "../assets/concoct.webp";
+import fab5logo from '../assets/fab5.webp';
 const Partners = () => {
   return (
     <div className={styles.partnersContainer}>
-      <h3>Ticketing Partner</h3>
-      <img src={makemypasslogo} alt="MakeMyPass Logo" className={styles.partnerLogo} />
+    <div className={styles.festivePartner}>
+        <h3>Festive Partner</h3>
+        <img src={concoctlogo} alt="Concoct Logo" className={styles.concoctLogo} />
+      </div>
+      <div className={styles.ticketingPartner}>
+        <h3>Ticketing Partner</h3>
+        <img src={makemypasslogo} alt="MakeMyPass Logo" className={styles.partnerLogo} />
+      </div>
+      <div className={styles.artistPartner}>
+        <h3>Artist Managed By</h3>
+        <img src={fab5logo} alt="Fab5 Logo" className={styles.fab5Logo} />
+      </div>
     </div>
   );
 }
